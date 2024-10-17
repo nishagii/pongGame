ce31a85d4a15390f03ee9465d89090b446a61964
nishagii <jeewanthadeherath04@gmail.com>
2024-10-17 20:39:00 +0530

    added ball class

--- pong/pong.py
+++ pong/pong.py
@@ -1,5 +1,6 @@
-from turtle import Screen
+from turtle import Screen,Turtle
 from paddle import Paddle
+from ball import Ball
 
 screen=Screen()
 
@@ -12,6 +13,7 @@ screen.tracer(0)#turn off animation
 
 right_paddle=Paddle((350,0))
 left_paddle=Paddle((-350,0))
+ball=Ball()
 
 
 screen.listen()
